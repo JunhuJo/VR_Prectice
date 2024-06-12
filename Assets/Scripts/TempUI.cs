a303b47bbf1312dc02c24e56bbc12abc300effbf
조준후 <jogusals4601@naver.com>
2024-06-12 15:16:57 +0900

    [Update] AssetChange

--- Assets/Scripts/TempUI.cs
+++ Assets/Scripts/TempUI.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +7,18 @@ public class TempUI : MonoBehaviour
 {
     [SerializeField] Button Btn_Temp;
     [SerializeField] Slider Slider_Temp;
+
+    [SerializeField] GameObject pensGate;
+    [SerializeField] Text OpenText;
+    [SerializeField] float MoveSpeed = 2.0f;
+    public bool open = false;
+    public bool move = false;
+
+    private void Update()
+    {
+        Open_Btn();
+        StartCoroutine(MoveGate());
+    }
 
     private void OnEnable()
     {
@@ -28,11 +40,32 @@ public class TempUI : MonoBehaviour
 
     private void OnClick_TemporalBtn()
     {
-        Debug.Log("버튼 눌러짐");
+        open = true;
+    }
+
+    void Open_Btn()
+    {
+        if(open)
+        Slider_Temp.value += 0.1f;
     }
 
     private void OnValueChanged_Slider(float value)
     {
-        Debug.Log($"슬라이더 변경 {value}");
+        if (Slider_Temp.value == 1)
+        {
+            OpenText.text = "완료";
+            move = true;
+        }
+    }
+
+   
+    IEnumerator MoveGate()
+    {
+        if (move)
+        {
+            pensGate.transform.Translate(Vector3.down * MoveSpeed * Time.deltaTime);
+            yield return new WaitForSeconds(5.0f);
+            pensGate.gameObject.SetActive(false);
+        }
     }
 }
